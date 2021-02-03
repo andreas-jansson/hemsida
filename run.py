4ba9d54a203c0affdd06b6c1584d75ca2a502bfd
andreas-jansson <andreas.jansson@protonmail.com>
2021-02-03 11:43:32 +0100

    1st commit

---
+++ run.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home2():
+    return render_template('home.html')
+
+@app.route("/jag")
+def home():
+    return render_template('jag.html')
+
+@app.route('/sida2')
+def sida2():
+    return render_template('sida2.html')
+
+
+
+
+#if __name__ == "__main__":
+#    app.run(debug=True)
+
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', debug = True)
